025df2225aef14cc53eec9602f83b9830eca941c
Johnson Kamau <Johnson Kamau@JOHNSON>
2024-03-09 14:37:33 +0300

    Add project files.

---
+++ ASG2.1/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtChem = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.txtPhy = New System.Windows.Forms.TextBox()
+        Me.txtOop = New System.Windows.Forms.TextBox()
+        Me.lblChem = New System.Windows.Forms.Label()
+        Me.lblPhy = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblOOP = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblTt = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblGD = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.lblAVG = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.lblGrade = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.btnCompute = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtChem
+        '
+        Me.txtChem.Location = New System.Drawing.Point(152, 69)
+        Me.txtChem.Name = "txtChem"
+        Me.txtChem.Size = New System.Drawing.Size(127, 20)
+        Me.txtChem.TabIndex = 0
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'txtPhy
+        '
+        Me.txtPhy.Location = New System.Drawing.Point(152, 117)
+        Me.txtPhy.Name = "txtPhy"
+        Me.txtPhy.Size = New System.Drawing.Size(127, 20)
+        Me.txtPhy.TabIndex = 0
+        '
+        'txtOop
+        '
+        Me.txtOop.Location = New System.Drawing.Point(152, 169)
+        Me.txtOop.MaximumSize = New System.Drawing.Size(500, 500)
+        Me.txtOop.MaxLength = 50000
+        Me.txtOop.Name = "txtOop"
+        Me.txtOop.Size = New System.Drawing.Size(127, 20)
+        Me.txtOop.TabIndex = 0
+        '
+        'lblChem
+        '
+        Me.lblChem.AutoSize = True
+        Me.lblChem.Location = New System.Drawing.Point(102, 72)
+        Me.lblChem.Name = "lblChem"
+        Me.lblChem.Size = New System.Drawing.Size(44, 13)
+        Me.lblChem.TabIndex = 2
+        Me.lblChem.Text = "CHEM :"
+        '
+        'lblPhy
+        '
+        Me.lblPhy.AutoSize = True
+        Me.lblPhy.Location = New System.Drawing.Point(111, 120)
+        Me.lblPhy.Name = "lblPhy"
+        Me.lblPhy.Size = New System.Drawing.Size(35, 13)
+        Me.lblPhy.TabIndex = 2
+        Me.lblPhy.Text = "PHY :"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Location = New System.Drawing.Point(568, 58)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(42, 13)
+        Me.lblTotal.TabIndex = 2
+        Me.lblTotal.Text = "TOTAL"
+        '
+        'lblOOP
+        '
+        Me.lblOOP.AutoSize = True
+        Me.lblOOP.Location = New System.Drawing.Point(111, 176)
+        Me.lblOOP.Name = "lblOOP"
+        Me.lblOOP.Size = New System.Drawing.Size(36, 13)
+        Me.lblOOP.TabIndex = 2
+        Me.lblOOP.Text = "OOP :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(898, 11)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Label1"
+        '
+        'lblTt
+        '
+        Me.lblTt.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTt.Location = New System.Drawing.Point(539, 71)
+        Me.lblTt.Name = "lblTt"
+        Me.lblTt.Size = New System.Drawing.Size(105, 30)
+        Me.lblTt.TabIndex = 2
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(902, 74)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Label1"
+        '
+        'lblGD
+        '
+        Me.lblGD.AutoSize = True
+        Me.lblGD.Location = New System.Drawing.Point(568, 185)
+        Me.lblGD.Name = "lblGD"
+        Me.lblGD.Size = New System.Drawing.Size(45, 13)
+        Me.lblGD.TabIndex = 2
+        Me.lblGD.Text = "GRADE"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(898, 114)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(39, 13)
+        Me.Label9.TabIndex = 2
+        Me.Label9.Text = "Label1"
+        '
+        'lblAVG
+        '
+        Me.lblAVG.AutoSize = True
+        Me.lblAVG.Location = New System.Drawing.Point(555, 124)
+        Me.lblAVG.Name = "lblAVG"
+        Me.lblAVG.Size = New System.Drawing.Size(58, 13)
+        Me.lblAVG.TabIndex = 2
+        Me.lblAVG.Text = "AVERAGE"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(898, 146)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(39, 13)
+        Me.Label11.TabIndex = 2
+        Me.Label11.Text = "Label1"
+        '
+        'lblGrade
+        '
+        Me.lblGrade.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGrade.Location = New System.Drawing.Point(539, 198)
+        Me.lblGrade.Name = "lblGrade"
+        Me.lblGrade.Size = New System.Drawing.Size(105, 29)
+        Me.lblGrade.TabIndex = 2
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(898, 182)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(39, 13)
+        Me.Label13.TabIndex = 2
+        Me.Label13.Text = "Label1"
+        '
+        'btnCompute
+        '
+        Me.btnCompute.Location = New System.Drawing.Point(219, 312)
+        Me.btnCompute.Name = "btnCompute"
+        Me.btnCompute.Size = New System.Drawing.Size(75, 23)
+        Me.btnCompute.TabIndex = 3
+        Me.btnCompute.Text = "COMPUTE"
+        Me.btnCompute.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(340, 312)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(462, 312)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblAverage
+        '
+        Me.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAverage.Location = New System.Drawing.Point(539, 137)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(105, 29)
+        Me.lblAverage.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 375)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCompute)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblGrade)
+        Me.Controls.Add(Me.lblAVG)
+        Me.Controls.Add(Me.lblGD)
+        Me.Controls.Add(Me.lblTt)
+        Me.Controls.Add(Me.lblOOP)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblPhy)
+        Me.Controls.Add(Me.lblChem)
+        Me.Controls.Add(Me.txtOop)
+        Me.Controls.Add(Me.txtPhy)
+        Me.Controls.Add(Me.txtChem)
+        Me.Name = "Form1"
+        Me.Text = "JOHNSON KAMAU DCS-01-8245/2023"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtChem As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents txtPhy As TextBox
+    Friend WithEvents txtOop As TextBox
+    Friend WithEvents lblChem As Label
+    Friend WithEvents lblPhy As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lblOOP As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblTt As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblGD As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents lblAVG As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents lblGrade As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents btnCompute As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblAverage As Label
+End Class
---
+++ ASG2.1/Form1.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Runtime.InteropServices.ComTypes
+
+Public Class Form1
+
+    Private Sub lblTt_Click(sender As Object, e As EventArgs) Handles lblTt.Click
+        Dim chem As Single, phy As Single, oop As Single, total As Single
+        chem = txtChem.Text
+        phy = txtPhy.Text
+        oop = txtOop.Text
+        total = Val(chem + phy + oop)
+        MessageBox.Show(total)
+
+
+    End Sub
+
+    Private Sub lblAverage_Click(sender As Object, e As EventArgs) Handles lblAverage.Click
+        Dim chem As Single, phy As Single, oop As Single, total As Single, average As Single
+        chem = txtChem.Text
+        phy = txtPhy.Text
+        oop = txtOop.Text
+        average = Val(total / 3)
+        MessageBox.Show(average)
+    End Sub
+
+    Private Sub lblGrade_Click(sender As Object, e As EventArgs) Handles lblGrade.Click
+        Dim average As Single, grade As String
+        If average >= 0 And average <= 29 Then
+            grade = MessageBox.Show("D")
+        ElseIf average >= 30 And average <= 59 Then
+            grade = MessageBox.Show("C")
+        ElseIf average >= 60 And average <= 79 Then
+            grade = MessageBox.Show("B")
+        ElseIf average >= 80 And average <= 100 Then
+            grade = MessageBox.Show("A")
+        Else
+            grade = MessageBox.Show("ERROR")
+        End If
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtChem.Clear()
+        txtPhy.Clear()
+        txtOop.Clear()
+        txtChem.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+        Dim chem As Single, phy As Single, oop As Single, total As Single, average As Single, grade As String
+        chem = txtChem.Text
+        phy = txtPhy.Text
+        oop = txtOop.Text
+        total = Val(chem + phy + oop)
+        average = Val(total / 3)
+        If average >= 0 And average <= 29 Then
+            grade = MessageBox.Show("You have a D")
+        ElseIf average >= 30 And average <= 59 Then
+            grade = MessageBox.Show("You have a C")
+        ElseIf average >= 60 And average <= 79 Then
+            grade = MessageBox.Show("You have a B")
+        ElseIf average >= 80 And average <= 100 Then
+            grade = MessageBox.Show("You have an A")
+        Else
+            grade = MessageBox.Show("ERROR")
+        End If
+
+        lblTt.Text = total.ToString()
+        lblAverage.Text = average.ToString("N2")
+        lblGrade.Text = grade
+
+
+
+    End Sub
+End Class
